261676f05b0ad898e1fbff3044c74185ffa01b3e
lasercata <67599917+lasercata@users.noreply.github.com>
2025-11-17 14:58:49 +0100

    Initial commit: create project structure

---
+++ README.md
@@ -0,0 +1,6 @@
+# Red-Nosed Reports (advent of code #2 2024)
+
+Todo:
+- description
+- Give the java version
+- Link: https://adventofcode.com/2024/day/2
---
+++ src/main/java/com/red_nosed_reports/main/MainRedNosedReports.java
@@ -0,0 +1,10 @@
+package com.red_nosed_reports.main;
+
+/**
+ * This is the main class running the project.
+ */
+public class MainRedNosedReports {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
